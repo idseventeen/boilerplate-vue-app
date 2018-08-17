ce0e37aa40a81f1a58752e1fd23f384ae26d8eb1
Shen7 <sq@hhdd.com>
2018-08-17 13:51:17 +0800

    eslint 规则配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,15 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
+  extends: [
     'plugin:vue/essential',
     '@vue/standard'
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-console': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'camelcase': 0,
+    'space-before-function-paren': 0
   },
   parserOptions: {
     parser: 'babel-eslint'
